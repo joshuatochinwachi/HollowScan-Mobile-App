ab7bc5512e438a17ecc77a8d49bb58ac033178c4
joshuatochinwachi <joshuatochinwachi@gmail.com>
2026-05-01 15:43:58 +0100

    v 1.0.3

--- app.config.js
+++ app.config.js
@@ -31,7 +31,7 @@ module.exports = ({ config }) => {
     ...config,
     name: "HollowScan",
     slug: "hollowscan_app",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
